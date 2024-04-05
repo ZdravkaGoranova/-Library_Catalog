147a38a4887180ff7b77c9e4edb8d649733089ca
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-04-05 12:27:43 +0300

    Modifile ui.test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -44,9 +44,10 @@ test('Verify That the "All Books" Link Is Visible after login', async ({ page })
     await page.fill('input[name="password"]', '123456');
     await page.click('input[type="submit"]');
 
-    const allBooksLink = await page.$('a[href="/catalog"] ');
-    const isAllBooksLinkIsVisible = await allBooksLink.isVisible();
-    expect(isAllBooksLinkIsVisible).toBe(true);
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+
+    expect(isAllBooksLinkVisible).toBe(true);
 
 });
 
@@ -86,13 +87,14 @@ test('Verify That the User\'s Email Address Is Visible', async ({ page }) => {
 });
 
 //Login  with Valid Credentials 
-test('Login  with Valid Credentials ', async ({ page }) => {
+test('Login with valid credentials', async ({ page }) => {
     await page.goto('http://localhost:3000/login');
-
-    await page.fill('#email', 'petar@abv.bg');
-    await page.fill('#password', '123456');
+  
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+  
     await page.click('input[type="submit"]');
-
+  
     await page.$('a[href="/catalog"]');
     expect(page.url()).toBe('http://localhost:3000/catalog');
 
@@ -100,19 +102,20 @@ test('Login  with Valid Credentials ', async ({ page }) => {
     // expect(page.url()).toBe('http://localhost:3000/catalog');
 });
 
-//Submit the Form with Empty Input Fields 
-test('Submit the Form with Empty Input Fields', async ({ page }) => {
+//Submit the Form with Empty Input Fields /Login with empty input fields
+test('Login with empty input fields', async ({ page }) => {
     await page.goto('http://localhost:3000/login');
     await page.click('input[type="submit"]');
-
+  
     page.on('dialog', async dialog => {
-        expect(dialog.type()).toContain('alert');
+        expect(dialog.type()).toContain('alert');   
         expect(dialog.message()).toContain('All fields are required!');
         await dialog.accept();
-    })
-    await page.$('a[href="/login"]');
-    expect(page.url()).toBe('http://localhost:3000/login');
-});
+      });
+  
+      await page.$('a[href="/login"]');
+      expect(page.url()).toBe('http://localhost:3000/login');
+  });
 
 //Submit the Form with Empty Email Input Field
 // test('', async ({ }) => { });
@@ -135,35 +138,34 @@ test('Submit the Form with Empty Input Fields', async ({ page }) => {
 
 //Add Book" Page
 
-//Submit the Form with Correct Data
-test('Submit the Form with Correct Data', async ({ page }) => {
+//Submit the Form with Correct Data/Add book with correct data
+test('Add book with correct data', async ({ page }) => {
 
     await page.goto('http://localhost:3000/login');
 
-    await page.fill('#email', 'petar@abv.bg');
-    await page.fill('#password', '123456');
-
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+  
     await Promise.all([
-        page.click('input[type="submit"]'),
-        page.waitForURL('http://localhost:3000/catalog',)
-
-    ])
+      page.click('input[type="submit"]'), 
+      page.waitForURL('http://localhost:3000/catalog')
+    ]);
+  
     await page.click('a[href="/create"]');
+  
     await page.waitForSelector('#create-form');
-
-
+  
     await page.fill('#title', 'Test Book');
     await page.fill('#description', 'This is a test book description');
-    await page.fill('#image', 'https://expample.com/book-image.jpj');
-
+    await page.fill('#image', 'https://example.com/book-image.jpg');
     await page.selectOption('#type', 'Fiction');
-
+  
     await page.click('#create-form input[type="submit"]');
-    await page.waitForURL('http://localhost:3000/catalog',)
-
-    expect(page.url()).toBe('http://localhost:3000/catalog')
-
-});
+  
+    await page.waitForURL('http://localhost:3000/catalog');
+  
+    expect(page.url()).toBe('http://localhost:3000/catalog');
+  });
 
 //Submit the Form with Empty Title Field
 test('Submit the Form with Empty Title Field', async ({ page }) => {
